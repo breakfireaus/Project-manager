439a7ce1a7ddfd3edcbd30bfc4484b0d079ff695
breakfireaus <myoungerc@hotmail.com>
2023-03-05 16:54:49 +1100

    added projects schema

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -7,6 +7,22 @@ import {
   GraphQLSchema,
 } from 'graphql';
 
+// Project type
+
+const ProjectType = new GraphQLObjectType({
+  name: 'Project',
+  fields: () => ({
+    id: { type: GraphQLID },
+    name: { type: GraphQLString },
+    description: { type: GraphQLString },
+    status: { type: GraphQLString },
+    client: ClientType,
+    resolve(parent, args) {
+      return clients.find((client) => client.id === args.id);
+    }
+  }),
+});
+
 // Client type
 
 const ClientType = new GraphQLObjectType({
@@ -22,6 +38,20 @@ const ClientType = new GraphQLObjectType({
 const RootQuery = new GraphQLObjectType({
   name: 'RootQueryType',
   fields: {
+    projects: {
+      type: new GraphQLList(ProjectType),
+      resolve(parents, args) {
+        return projects;
+      },
+    },
+    project: {
+      type: ProjectType,
+      args: { id: { type: GraphQLID } },
+      resolve(parent, args) {
+        return projects.find((project) => project.id === args.id);
+      },
+    },
+
     clients: {
       type: new GraphQLList(ClientType),
       resolve(parents, args) {
