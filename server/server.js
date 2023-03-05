a19c6f9da998d815de9fa7cafb4f8d8006663c69
breakfireaus <myoungerc@hotmail.com>
2023-03-05 17:12:49 +1100

    configured mongodb server and connected to db

--- server/server.js
+++ server/server.js
@@ -1,15 +1,23 @@
 import express from 'express';
+import colors from 'colors';
 require('dotenv').config();
 import { graphQlHTTP } from 'express-graphql';
 import schema from './schema/schema';
+import connectDB from './config/db';
 
 const port = process.env.PORT || 5000;
 
 const app = express();
 
-app.use('/graphql', graphQlHTTP({
+// connect to DATABASE
+connectDB();
+
+app.use(
+  '/graphql',
+  graphQlHTTP({
     schema,
-    graphiql: process.env.NODE_ENV === 'development'
-}));
+    graphiql: process.env.NODE_ENV === 'development',
+  })
+);
 
 app.listen(port, console.log(`Server running on port ${port}`));
